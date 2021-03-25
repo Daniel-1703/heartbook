4f7004dbffb77519a221b7a44e08ad02a79cdbd3
Daniil <d.daniil17.mart@gmail.com>
2021-03-26 00:36:55 +0300

    add styles and the ability to like and add to favorites

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,8 +5,16 @@ import SearchPanel from "../search-panel/search-panel";
 import PostStatusFilter from "../post-status-filter/post-status-filter";
 import PostList from "../post-list/post-list";
 import PostAddForm from "../post-add-form/post-add-form";
+import './app.css';
 
 const App = () => {
+
+    const data = [
+        {label: "I like React", important: true, id: 'qwqw'},
+        {label: "My favorite activity is developing applications", important: false, id: 'gfds'},
+        {label: "Frontend is cool", important: false, id: 'fdfdf'}
+    ];
+
     return (
         <div className="app">
             <AppHeader/>
@@ -14,7 +22,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
         </div>
     )
