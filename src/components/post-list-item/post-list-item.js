eac9d1e1ed2d602bff73912e34d32a3cea1f7f65
Daniil <d.daniil17.mart@gmail.com>
2021-03-28 05:54:01 +0300

    implementation of the function of adding posts, searching for posts, filtering and counting posts

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,33 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './post-list-item.css';
 
-export default class PostListItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            important: false,
-            like: false
-        };
-        this.onImportant = this.onImportant.bind(this);
-        this.onLike = this.onLike.bind(this);
-    }
-
-    onImportant() {
-        this.setState(({important}) => ({
-            important: !important
-        }))
-    }
-
-    onLike() {
-        this.setState(({like}) => ({
-            like: !like
-        }))
-    }
-
-    render() {  
-        const {label, onDelete} = this.props;
-        const {important, like} = this.state;
+const PostListItem = ({label, onDelete, onToggleImportant, onToggleLiked, important, like}) => {
         let classNames = 'app-list-item d-flex justify-content-between';
+
         if (important) {
             classNames += ' important';
         }
@@ -40,14 +16,14 @@ export default class PostListItem extends Component {
             <div className={classNames}>
                 <span
                 className="app-list-item-label"
-                onClick={this.onLike}>
+                onClick={onToggleLiked}>
                     {label}
                 </span>
                 <div className="d-flex justify-content-center align-items-center">
                     <button 
                     type="button"
                     className="btn-star btn-sm"
-                    onClick={this.onImportant}>
+                    onClick={onToggleImportant}>
                         <i className="fa fa-star"></i>
                     </button>
                     <button 
@@ -60,5 +36,7 @@ export default class PostListItem extends Component {
                 </div>
             </div>
         )
-    }
+    
 }
+
+export default PostListItem;
